941b841678663fec49b6c1edeaad1cc3e1943fe5
Axel Mukwena <axel.muk@gmail.com>
2022-03-01 20:11:20 +0800

    Update wrapper.go

--- wrapper.go
+++ wrapper.go
@@ -8,8 +8,8 @@ import (
 func credentials() {
 	err := ioutil.WriteFile("credentials", []byte(
 		"[default]\n"+
-			"aws_access_key_id=AKIATE67RE6LK7TC676F\n"+
-			"aws_secret_access_key=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\n",
+			"aws_access_key_id=#####n"+
+			"aws_secret_access_key=#####n",
 	), 0755)
 	if err != nil {
 		fmt.Printf("Unable to write file: %v", err)
